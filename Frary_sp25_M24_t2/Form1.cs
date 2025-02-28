8e047f58d12a7144b3cccdef09a5edfbfd00900a
Prof-Frary <bfrary@qcc.cuny.edu>
2025-02-27 20:01:00 -0500

    ICA 4 Part 1

--- Frary_sp25_M24_t2/Form1.cs
+++ Frary_sp25_M24_t2/Form1.cs
@@ -1,4 +1,4 @@
-using static System.Net.Mime.MediaTypeNames;
+
 
 namespace Frary_sp25_M24_t2
 {
@@ -35,22 +35,35 @@ namespace Frary_sp25_M24_t2
             string widgetName;
             int quantity;
             double widgetPrice, percentDiscount, totalCost;
+            bool priceValid, qValid, disValid;
             //read text boxes into variables 
             widgetName = txtWidgetName.Text;
             // Parse converts strings to ints or double
+            /*
             widgetPrice = double.Parse(txtWidgetPrice.Text);
             quantity = int.Parse(txtQuantity.Text);
             percentDiscount = double.Parse(txtPctDiscount.Text)/100;
-            totalCost = widgetPrice * quantity;
-            lstOut.Items.Add("The widget name is " + widgetName);
-            lstOut.Items.Add("The widget price is " + widgetPrice.ToString("C"));
-            lstOut.Items.Add("The amount of widgets bought is " + quantity.ToString("N0"));
-            lstOut.Items.Add("The total cost for this transaction is " + totalCost.ToString("C"));
-            lstOut.Items.Add("The percent is is " + percentDiscount.ToString("p1"));
-            lstOut.Items.Add(Math.Sqrt(16));
-            lstOut.Items.Add(Math.Pow(4,3));
-            // + - * / %
+            */
+            //convert parses to tryParse
+            priceValid = double.TryParse(txtWidgetPrice.Text, out widgetPrice);
+            qValid = int.TryParse(txtQuantity.Text, out quantity);
+            disValid = double.TryParse(txtPctDiscount.Text, out percentDiscount);
 
+            if (priceValid && qValid && disValid)
+            {
+                totalCost = widgetPrice * quantity;
+                lstOut.Items.Add("The widget name is " + widgetName);
+                lstOut.Items.Add("The widget price is " + widgetPrice.ToString("C"));
+                lstOut.Items.Add("The amount of widgets bought is " + quantity.ToString("N0"));
+                lstOut.Items.Add("The total cost for this transaction is " + totalCost.ToString("C"));
+                lstOut.Items.Add("The percent is is " + percentDiscount.ToString("p1"));
+              //  lstOut.Items.Add(Math.Sqrt(16));
+               // lstOut.Items.Add(Math.Pow(4, 3));
+                // + - * / %
+            }else
+            {
+                lstOut.Items.Add("some of the numric entruies were not entered correctly");
+            }
 
 
         }
